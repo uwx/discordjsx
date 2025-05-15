859d75ed9c4849760ccd4afd5c39b97635bdd420
uwx <maxine@cybergal.com>
2025-05-15 19:51:49 -0300

    Tiny nitpick fixes
    (cherry picked from commit 8956ada89a83c51622a9da39b725001c013d902d)

--- src/payload/index.ts
+++ src/payload/index.ts
@@ -125,14 +125,14 @@ export class PayloadBuilder {
                 };
             case "text":
                 return {
-                    type: 10,
+                    type: ComponentType.TextDisplay,
                     content: this.getText(node),
                 };
             case "thumbnail":
                 if (!node.props.media) return null;
 
                 return {
-                    type: 11,
+                    type: ComponentType.Thumbnail,
                     media: typeof node.props.media === 'string' ? { url: node.props.media } : node.props.media,
                     description: node.props.description,
                     spoiler: node.props.spoiler,
@@ -141,7 +141,7 @@ export class PayloadBuilder {
                 if (!node.children) return null;
 
                 return {
-                    type: 12,
+                    type: ComponentType.MediaGallery,
                     items: node.children
                         .filter(child => child.type === 'gallery-item')
                         .map(child => {
@@ -155,19 +155,19 @@ export class PayloadBuilder {
                 };
             case "file":
                 return {
-                    type: 13,
+                    type: ComponentType.File,
                     file: typeof node.props.file === 'string' ? { url: node.props.file } : node.props.file,
                     spoiler: node.props.spoiler,
                 };
             case "separator":
                 return {
-                    type: 14,
+                    type: ComponentType.Separator,
                     divider: node.props.divider,
-                    spacing: node.props.spacing == "lg" ? 2 : 1,
+                    spacing: node.props.spacing === "lg" ? 2 : 1,
                 };
             case "container":
                 return {
-                    type: 17,
+                    type: ComponentType.Container,
                     components: this.toDiscordComponentsArray(node.children) as any,
                     accent_color: node.props.color ? resolveColor(node.props.color) : undefined,
                     spoiler: node.props.spoiler,
@@ -182,13 +182,13 @@ export class PayloadBuilder {
         if(!c) throw new Error();
 
         if(
-            c.type == ComponentType.StringSelect
-            || c.type == ComponentType.UserSelect
-            || c.type == ComponentType.RoleSelect
-            || c.type == ComponentType.MentionableSelect
-            || c.type == ComponentType.ChannelSelect
-            || c.type == ComponentType.Button
-            || c.type == ComponentType.Thumbnail
+            c.type === ComponentType.StringSelect
+            || c.type === ComponentType.UserSelect
+            || c.type === ComponentType.RoleSelect
+            || c.type === ComponentType.MentionableSelect
+            || c.type === ComponentType.ChannelSelect
+            || c.type === ComponentType.Button
+            || c.type === ComponentType.Thumbnail
         ) throw new Error();
 
         return c;
@@ -232,7 +232,7 @@ export class PayloadBuilder {
             max_values: node.props.max,
             disabled: this.everythingDisabled ? true : node.props.disabled,
             placeholder: node.props.placeholder,
-            ...(node.props.type == "string" ? {
+            ...(node.props.type === "string" ? {
                 options: node.children.map(child => ({
                     label: (child as InstrinsicNodesMap["option"]).props.label,
                     value: (child as InstrinsicNodesMap["option"]).props.value,
@@ -241,13 +241,13 @@ export class PayloadBuilder {
                     default: (node.props.defaultValues as string[] | undefined)?.includes((child as InstrinsicNodesMap["option"]).props.value),
                 })),
             } : {}),
-            ...(node.props.type == "user" || node.props.type == "role" ? {
+            ...(node.props.type === "user" || node.props.type === "role" ? {
                 default_values: node.props.defaultValues?.map(id => ({ id, type: node.props.type })) as any,
             } : {}),
-            ...(node.props.type == "mentionable" ? {
+            ...(node.props.type === "mentionable" ? {
                 default_values: node.props.defaultValues as any,
             } : {}),
-            ...(node.props.type == "channel" ? {
+            ...(node.props.type === "channel" ? {
                 channel_types: node.props.channelTypes,
                 default_values: node.props.defaultValues?.map(id => ({ id, type: "channel" })) as any,
             } : {}),
